3cd2c3797d2cff2915aba26cd885050dc9d9af97
Daniel <binaryclock03@gmail.com>
2023-03-05 22:18:14 -0500

    added more console outputs to run

--- run.py
+++ run.py
@@ -1,10 +1,20 @@
 import file_reader as fr
 import plotter
 
+linespace = "-------------"
+
+print(linespace)
+print("Loading Model")
 model = fr.load_model("setups/setup_project.yaml")
+print("Load Complete")
+
+print(linespace)
+print("Solving model")
 model.solve()
 solved_model = model.return_solved_model(scale=500)
+print("Solving complete")
 
+print(linespace)
 print("Mass Efficiency: " + str(9000/model.find_mass()))
 
 plotter.plot_model_setup(model)
